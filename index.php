2b09085987c08a421d0651e24143fbecfa496556
guneskahil <guneskahill@gmail.com>
2024-04-30 14:49:54 +0300

    id eklendi

--- index.php
+++ index.php
@@ -20,7 +20,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         if ($kullanici) {
             // Kullanıcı bulundu, giriş yap
             $_SESSION['ad'] = $kullanici['ad']; // Kullanıcının adını oturum verilerine kaydet
-            header("Location: index.php"); // Ana sayfaya yönlendir
+            $_SESSION['kimlik_no'] = $kullanici['kimlik_no']; // Kimlik numarasını oturum verilerine kaydet
+            $_SESSION['soyad'] = $kullanici['soyad'];
+            $_SESSION['kullanici_id'] = $kullanici['kullanici_id'];
+            $_SESSION['mail'] = $kullanici['mail'];
+            $_SESSION['tel_no'] = $kullanici['tel_no']; // Kullanıcının adını oturum verilerine kaydet
+            header("Location: anaSayfa.php"); // Ana sayfaya yönlendir
             exit(); // Yönlendirme yapıldıktan sonra kodun devamını çalıştırmamak için exit kullanılmalı
         } else {
             // Kullanıcı bulunamadı, hata mesajı ayarla
@@ -40,11 +45,6 @@ if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET['action']) && $_GET['acti
     header("Location: index.php");
     exit();
 }
-
-
-
-
-
 ?>
 
 
@@ -79,11 +79,6 @@ if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET['action']) && $_GET['acti
 </head>
 
 <body>
-    <!-- db connection start-->
-
-
-    <!-- db connection end-->
-
 
     <!-- Topbar Start -->
     <div class="container-fluid bg-light pt-3 d-none d-lg-block">
@@ -97,7 +92,7 @@ if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET['action']) && $_GET['acti
         <div class="container-lg position-relative p-0 px-lg-3" style="z-index: 9;">
             <nav class="navbar navbar-expand-lg bg-light navbar-light shadow-lg py-3 py-lg-0 pl-3 pl-lg-5">
                 <img class="img-fluid" src="img/inekikon.png" style="height: 8%; width: 8%;" alt="">
-                <a href="index.php" class="navbar-brand">
+                <a href="" class="navbar-brand">
                     <h1 class="m-0 text-primary"><span class="text-dark">Lakta</span>Farm</h1>
                 </a>
                 <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
@@ -110,19 +105,20 @@ if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET['action']) && $_GET['acti
                         <div class="nav-item dropdown">
                             <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Döngüler</a>
                             <div class="dropdown-menu border-0 rounded-0 m-0">
-                                <a href="servisPeriyoduMetin.php" class="dropdown-item">Servis Periyodu</a>
-                                <a href="kuruDonemMetin.php" class="dropdown-item">Kuru Dönem Periyodu</a>
-                                <a href="sagimMetin.php" class="dropdown-item">Sağım Periyodu</a>
-                                <a href="gebelikMetin.php" class="dropdown-item">Gebelik Periyodu</a>
+                                <a href="service.php" class="dropdown-item">Pervis Periyodu</a>
+                                <a href="single.html" class="dropdown-item">Kuru Dönem</a>
+                                <a href="destination.html" class="dropdown-item">Sağım</a>
+                                <a href="guide.html" class="dropdown-item">Gebelik</a>
                             </div>
                         </div>
                         <div class="nav-item dropdown">
                             <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">İnekler</a>
                             <div class="dropdown-menu border-0 rounded-0 m-0">
-                                <a href="gunlukKontrol.php" class="dropdown-item">Günlük Takip</a>
-                                <a href="dollemeTakip.php" class="dropdown-item">Dölleme Takip</a>
-                                <a href="gebeTakip.php" class="dropdown-item">Gebe Takip</a>
-                                <a href="kuruDonemTakip.php" class="dropdown-item">Kuru Dönem Takip</a>
+                                <a href="gunlukKontrol.html" class="dropdown-item">Günlük Takip</a>
+                                <a href="dollemeTakip.html" class="dropdown-item">Dölleme Takip</a>
+                                <a href="destination.html" class="dropdown-item">Gebe Takip</a>
+                                <a href="single.php" class="dropdown-item">Kuru Dönem Takip</a>
+                                <a href="inekKayit.html" class="dropdown-item">İnek Kayıt</a>
                             </div>
                         </div>
                         <?php if (isset($_SESSION['ad'])): ?>
@@ -131,7 +127,7 @@ if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET['action']) && $_GET['acti
                                     data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                     Merhaba, <?php echo $_SESSION['ad']; ?>
                                 </a>
-                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <div class="dropdown-menu border-0 rounded-0 m-0" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="profil.php">Profilim</a>
                                     <a class="dropdown-item" href="?action=logout">Çıkış Yap</a>
                                 </div>
@@ -148,16 +144,16 @@ if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET['action']) && $_GET['acti
     <!-- Navbar End -->
 
     <!-- Modal Start -->
-    <div id="myModal" class="modal" >
-        <div class="modal-content" style="border-radius: 20px !important;">
+    <div id="myModal" class="modal">
+        <div class="modal-content" style="border-radius:20px !important">
             <span class="close" onclick="closeAndResetModal('myModal')">×</span>
-            <h2 style="text-align: center " onclick="">Giriş Yap</h2>
+            <h2 style="text-align: center" onclick="">Giriş Yap</h2>
             <!-- Hata Mesajı için -->
             <p id="loginError" style="text-align: center; color: red;"><?php echo isset($error) ? $error : ''; ?></p>
             <form class="login-form" action="index.php" method="POST">
                 <div class="input-group" style="margin-bottom: 5px;">
-                    <label for="mail" style="display: block; margin-bottom: 5px; ">Mail Adresi:</label>
-                    <input type="text" id="mail" name="mail" style="width: 100%; padding: 8px; ">
+                    <label for="mail" style="display: block; margin-bottom: 5px;">Mail Adresi:</label>
+                    <input type="text" id="mail" name="mail" style="width: 100%; padding: 8px;">
                 </div>
 
                 <div class="input-group" style="margin-bottom: 5px;">
@@ -189,10 +185,12 @@ if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET['action']) && $_GET['acti
         });
 
         // Modal kapatma fonksiyonu ve hata mesajını temizleme
+
         function closeAndResetModal(modalId) {
             document.getElementById(modalId).style.display = "none";
-            document.getElementById('loginError').innerText = '';
+            document.getElementById('editInfoError').innerText = ''; // Hata mesajını sıfırla
         }
+
     </script>
 
 
@@ -272,8 +270,8 @@ if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET['action']) && $_GET['acti
 
 
 
-  <!-- Footer Start -->
-  <div class="container-fluid bg-dark text-white-50 py-5 px-sm-3 px-lg-5" style="margin-top: 90px;">
+    <!-- Footer Start -->
+    <div class="container-fluid bg-dark text-white-50 py-5 px-sm-3 px-lg-5" style="margin-top: 90px;">
         <div class="row pt-5">
             <div class="col-lg-3 col-md-6 mb-5">
                 <a href="" class="navbar-brand">
@@ -285,20 +283,20 @@ if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET['action']) && $_GET['acti
             <div class="col-lg-3 col-md-6 mb-5">
                 <h5 class="text-white text-uppercase mb-4" style="letter-spacing: 5px;">Döngüler</h5>
                 <div class="d-flex flex-column justify-content-start">
-                    <a class="text-white-50 mb-2" href="servisPeriyoduMetin.php"><i class="fa fa-angle-right mr-2"></i>Servis Periyodu</a>
-                    <a class="text-white-50 mb-2" href="kuruDonemMetin.php"><i class="fa fa-angle-right mr-2"></i>Kuru Dönem Periyodu</a>
-                    <a class="text-white-50 mb-2" href="sagimMetin.php"><i class="fa fa-angle-right mr-2"></i>Sağım Periyodu</a>
-                    <a class="text-white-50 mb-2" href="gebelikMetin.php"><i class="fa fa-angle-right mr-2"></i>Gebelik Periyodu</a>
+                    <a class="text-white-50 mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Pervis Periyodu</a>
+                    <a class="text-white-50 mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Kuru Dönem</a>
+                    <a class="text-white-50 mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Sağım</a>
+                    <a class="text-white-50 mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Gebelik</a>
                 </div>
             </div>
             <div class="col-lg-3 col-md-6 mb-5">
                 <h5 class="text-white text-uppercase mb-4" style="letter-spacing: 5px;">İnekler</h5>
                 <div class="d-flex flex-column justify-content-start">
-                    <a class="text-white-50 mb-2" href="gunlukKontrol.php"><i class="fa fa-angle-right mr-2"></i>Günlük Takip</a>
-                    <a class="text-white-50 mb-2" href="gebeTakip.php"><i class="fa fa-angle-right mr-2"></i>Gebelik Takip</a>
-                    <a class="text-white-50 mb-2" href="dollemeTakip.php"><i class="fa fa-angle-right mr-2"></i>Dölleme Takip</a>
-                    <a class="text-white-50 mb-2" href="kuruDonemTakip.php"><i class="fa fa-angle-right mr-2"></i>Kuru Dönem Takip</a>
-                    <a class="text-white-50 mb-2" href="inekKayit.php"><i class="fa fa-angle-right mr-2"></i>İnek Kayıt</a>
+                    <a class="text-white-50 mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Günlük Takip</a>
+                    <a class="text-white-50 mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Gebe Takip</a>
+                    <a class="text-white-50 mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Dölleme Takip</a>
+                    <a class="text-white-50 mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Kuru Dönem Takip</a>
+                    <a class="text-white-50 mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>İnek Kayıt</a>
                 </div>
             </div>
             <div class="col-lg-3 col-md-6 mb-5">
